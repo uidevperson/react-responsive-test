7647058a3df952bc6c68809609d5d6e3527991aa
uidevperson <32483505+uidevperson@users.noreply.github.com>
2018-08-20 16:26:15 +0100

    messing with different platform types

--- src/components/App.js
+++ src/components/App.js
@@ -3,35 +3,23 @@ import React, { Component } from 'react';
 import Footer from './Footer';
 import AddTodo from '../containers/AddTodo';
 import VisibleTodoList from '../containers/VisibleTodoList';
-import MediaQuery from 'react-responsive';
+import Responsive from 'react-responsive';
 import "./App.css";
+import Todo from './Todo';
+
+const Desktop = props => <Responsive {...props} minWidth={992} />;
+const Tablet = props => <Responsive {...props} minWidth={768} maxWidth={991} />;
+const Mobile = props => <Responsive {...props} maxWidth={767} />;
+const Default = props => <Responsive {...props} minWidth={768} />;
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <div>Device Test!</div>
-        <MediaQuery minDeviceWidth={1224}>
-          <div>You are a desktop or laptop</div>
-          <MediaQuery minDeviceWidth={1824}>
-            <div>You also have a huge screen</div>
-          </MediaQuery>
-          <MediaQuery maxWidth={1224}>
-            <div>You are sized like a tablet or mobile phone though</div>
-          </MediaQuery>
-        </MediaQuery>
-        <MediaQuery maxDeviceWidth={1224}>
-          You are a tablet or mobile phone
-        </MediaQuery>
-        <MediaQuery orientation='portrait'>
-          <div>You are portrait</div>
-        </MediaQuery>
-         <MediaQuery orientation='landscape'>
-          <div>You are landscape</div>
-        </MediaQuery>
-        <MediaQuery minResolution='2dppx'>
-          <div>You are retina</div>
-        </MediaQuery>
+        <Desktop>Desktop or laptop</Desktop>
+        <Tablet>Tablet</Tablet>
+        <Mobile>Mobile</Mobile>
+        <Default>Not mobile (desktop or laptop or tablet)</Default>
       </div>
     )
   }
