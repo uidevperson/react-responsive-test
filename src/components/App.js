13f3328b031e2591435aa8473db243a2ff1270cb
uidevperson <32483505+uidevperson@users.noreply.github.com>
2018-08-20 16:08:29 +0100

    init

---
+++ src/components/App.js
@@ -0,0 +1,40 @@
+// Presentational Component
+import React, { Component } from 'react';
+import Footer from './Footer';
+import AddTodo from '../containers/AddTodo';
+import VisibleTodoList from '../containers/VisibleTodoList';
+import MediaQuery from 'react-responsive';
+import "./App.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>Device Test!</div>
+        <MediaQuery minDeviceWidth={1224}>
+          <div>You are a desktop or laptop</div>
+          <MediaQuery minDeviceWidth={1824}>
+            <div>You also have a huge screen</div>
+          </MediaQuery>
+          <MediaQuery maxWidth={1224}>
+            <div>You are sized like a tablet or mobile phone though</div>
+          </MediaQuery>
+        </MediaQuery>
+        <MediaQuery maxDeviceWidth={1224}>
+          You are a tablet or mobile phone
+        </MediaQuery>
+        <MediaQuery orientation='portrait'>
+          <div>You are portrait</div>
+        </MediaQuery>
+         <MediaQuery orientation='landscape'>
+          <div>You are landscape</div>
+        </MediaQuery>
+        <MediaQuery minResolution='2dppx'>
+          <div>You are retina</div>
+        </MediaQuery>
+      </div>
+    )
+  }
+}
+
+export default App
